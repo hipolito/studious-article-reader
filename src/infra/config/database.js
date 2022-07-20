9685b51d810272338ed408d28220910ec476f6f6
Hipólito Moreira <hipolitodouglas@gmail.com>
2022-07-20 09:01:23 +0100

    adding default configuration for development purposes

--- src/infra/config/database.js
+++ src/infra/config/database.js
@@ -1,5 +1,13 @@
 module.exports = {
     development: {
+        database: process.env.DATABASE_NAME || 'studious_article_reader_db',
+        username: process.env.DATABASE_USER || 'root',
+        password: process.env.DATABASE_PASSWORD || 'mysqlpw',
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: process.env.DATABASE_PORT || 49154,
+        dialect: process.env.DATABASE_DIALECT || 'mysql'
+    },
+    production: {
         database: process.env.DATABASE_NAME,
         username: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
--- src/infra/config/server.js
+++ src/infra/config/server.js
@@ -1,3 +1,3 @@
 module.exports = {
-    port: process.env.SERVER_PORT
+    port: process.env.SERVER_PORT || 3000
 }
